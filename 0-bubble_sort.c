83d6bf988cd6c5227486b4738564ad21e1ba4078
chimekinglsey <onlinekingsley@gmail.com>
2023-04-22 09:21:52 +0000

    First commit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,23 +12,23 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j, temp;
 
 	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
 		{
-			for (j = 0; j < size; j++)
+			if (array[j + 1])
 			{
-				if (array[j + 1])
+				if (array[j] > array[j + 1])
 				{
-					if (array[j] > array[j + 1])
-					{
-						temp = array[j];
-						array[j] = array[j + 1];
-						array[j + 1] = temp;
-						print_array(array, size);
-					}
-				}
-				else 
-				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
 					print_array(array, size);
 				}
 			}
+			else
+			{
+				print_array(array, size);
+			}
 		}
+	}
 }
