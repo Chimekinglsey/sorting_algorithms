57d5f8b87b569249d6880a1359057b0e300095c5
chimekinglsey <onlinekingsley@gmail.com>
2023-04-25 05:40:45 +0000

    New commit

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -24,7 +24,7 @@ void selection_sort(int *array, size_t size)
 				tmp = array[i];
 				array[i] = array[key];
 				array[key] = tmp;
+				print_array(array, size);
 			}
-		print_array(array, size);
 	}
 }
