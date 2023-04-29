c843d55b4fa6da73548cfaabd7eeb392dfc3513e
milemotigo <mtigo3617@gmail.com>
2023-04-29 09:49:29 -0700

    commit

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,33 +1,46 @@
 #include "sort.h"
 
 /**
-* bubble_sort - this function sorts an array of integers in ascending order
+ * swap_ints - a function to swap two integers
+ * @a: First integer to swap
+ * @b: second integer to swap
+ */
+void swap_ints(int *a, int *b)
+{
+        int temp;
+        temp = *a;
+        *a = *b;
+        *b = temp;
+}
+
+/**
+* shell_sort - this function sorts an array of integers in ascending order
 * @array: the array to sort
 * @size: size of array to sort
 * Return - Nothing
 */
+
 void shell_sort(int *array, size_t size)
 {
-	size_t i, j, temp;
+        size_t space = 1, i,j;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size - 1; j++)
-		{
-			if (array[j + 1])
-			{
-				if (array[j] > array[j + 1])
-				{
-					temp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = temp;
-					print_array(array, size);
-				}
-			}
-			else
-			{
-				print_array(array, size);
-			}
-		}
-	}
+        if (array == NULL || size < 2)
+                return;
+
+        while (space < size / 3)
+                space = space * 3 + 1;
+	
+	for (space = size / 3; space >= 1; space /= 3)
+        {
+                for (j = space; j < size; j++)
+                {
+                        i = j;
+                        while (i >= space && array[i - space] > array[i])
+                        {
+                                swap_ints(&array[i], &array[i - space]);
+                                i = i - space;
+                        }
+                }
+                                print_array(array, size);
+        }
 }
